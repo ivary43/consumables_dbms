46aa097fe23e218f1b7fb7c5359b6625274437b7
ivary43 <contactmanish16@gmail.com>
2018-10-18 14:49:36 +0530

    refactored faculty model

--- models/Faculty.js
+++ models/Faculty.js
@@ -5,12 +5,25 @@ let {mongoose} = require('../db/mongoose');
 var facultySchema = new mongoose.Schema({
     name: {
         type: String,
+        minlength:3,
+        trim:true,
         required: true
     },
     isAdmin: {
         type: Boolean,
         default: false
+    }, username : {
+        type: String,
+        minlength: 4,
+        trim: true,
+        required: true
+    }, password: {
+        type: String,
+        minlength: 6,
+        trim: true,
+        required: true
     }
+
 });
 
 
@@ -18,4 +31,5 @@ var facultySchema = new mongoose.Schema({
 facultySchema.methods.isUserAdmin = function(tokenId) {
   return false ;
 };
+
 module.exports = mongoose.model("faculty", facultySchema);
