8d41274cad5c593a03dde2955e93a6d9ff8fa653
ivary43 <contactmanish16@gmail.com>
2018-11-06 19:13:46 +0530

    fix: added config.json

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -1,6 +1,8 @@
 'use strict';
 const nodemailer = require('nodemailer');
 const env_vars = require('./Constants');
+var jsonPass = require('../config/config_val');
+
 // Only needed if you don't have a real mail account for testing
 
     // create reusable transporter object using the default SMTP transport
@@ -10,7 +12,7 @@ const env_vars = require('./Constants');
         secure: false, // true for 465, false for other ports
         auth: {
             user: 'manish.cs16@iitp.ac.in', // generated ethereal user
-            pass: '' // generated ethereal password
+            pass: jsonPass.password // generated ethereal password
         }
     });
 
